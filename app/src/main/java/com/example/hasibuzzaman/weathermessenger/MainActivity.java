bb7df385f290281311d3b758b3110212b18070ac
The A team <theateamgit@gmail.com>
2016-08-06 03:48:20 +0600

    last

--- app/src/main/java/com/example/hasibuzzaman/weathermessenger/MainActivity.java
+++ app/src/main/java/com/example/hasibuzzaman/weathermessenger/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.hasibuzzaman.weathermessenger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -14,12 +17,89 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     WeatherServiceApi weatherServiceApi;
+    private AutoCompleteTextView autoCompleteTextView;
+    ArrayList<String> list = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         networkLibraryPopulate();
         loadDatas();
+
+
+        autoCompleteTextView= (AutoCompleteTextView) findViewById(R.id.autocompletetextView);
+        list.add(autoCompleteTextView.getText().toString());
+
+        list.add("Barguna");
+        list.add("Barisal");
+        list.add("Bhola");
+        list.add("Jhalokati");
+        list.add("Patuakhali");
+        list.add("Pirojpur");
+        list.add("Bandarban");
+        list.add("Brahmanbaria");
+        list.add("Chandpur");
+        list.add("Chittagong");
+        list.add("Comilla");
+        list.add("Cox's Bazar");
+        list.add("Feni");
+        list.add("Khagrachhari");
+        list.add("Lakshmipur");
+        list.add("Noakhal");
+        list.add("Rangamati");
+        list.add("Dhaka");
+        list.add("Faridpur");
+        list.add("Gazipur");
+        list.add("Gopalganj");
+        list.add("Jamalpur ");
+        list.add("Kishoreganj");
+        list.add("Madaripur");
+        list.add("Manikganj");
+        list.add("Munshiganj");
+        list.add("Mymensingh");
+        list.add("Narayanganj");
+        list.add("Narsingdi");
+        list.add("Netrakona");
+        list.add("Rajbari");
+        list.add("Shariatpur");
+        list.add("Sherpur");
+        list.add("Tangail");
+        list.add(" Bagerhat");
+        list.add("Chuadanga");
+        list.add("Jessore");
+        list.add("Jhenaidah");
+        list.add("Khulna");
+        list.add("Kushtia");
+        list.add("Magura");
+        list.add("Meherpur");
+        list.add("Narail");
+        list.add("Satkhira");
+        list.add("Bogra");
+        list.add("Joypurhat");
+        list.add("Naogaon");
+        list.add("Natore");
+        list.add("Nawabganj");
+        list.add("Pabna");
+        list.add("Rajshahi");
+        list.add("Sirajganj");
+        list.add("Dinajpur");
+        list.add("Gaibandha");
+        list.add("Kurigram");
+        list.add("Lalmonirhat");
+        list.add("Nilphamari");
+        list.add("Panchagarh");
+        list.add("Rangpur");
+        list.add("Thakurgaon");
+        list.add("Habiganj");
+        list.add("Moulvibazar");
+        list.add("Sunamganj");
+        list.add("Sylhet");
+
+        ArrayAdapter adapter=new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,list);
+        autoCompleteTextView.setAdapter(adapter);
+
+
+
     }
 
     private void loadDatas() {
@@ -44,8 +124,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void networkLibraryPopulate() {
-     Retrofit retrofit = new Retrofit.Builder().baseUrl(Constants.BASE_URL)
-             .addConverterFactory(GsonConverterFactory.create()).build();
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create()).build();
 
         weatherServiceApi = retrofit.create(WeatherServiceApi.class);
 
