3af0a94aae411c06f4940d9ff065ebbc2d9d685b
TheATeamGit <theateamgit@gmail.com>
2016-08-06 16:13:59 +0600

    changing Orientation 2

--- app/src/main/java/com/example/hasibuzzaman/weathermessenger/MainActivity.java
+++ app/src/main/java/com/example/hasibuzzaman/weathermessenger/MainActivity.java
@@ -20,7 +20,16 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
+
     WeatherServiceApi weatherServiceApi;
+
+
+    WeatherResponse weatherResponse ;  //Contains the Whole object that comes from the API
+    List<WeatherResponse.Weather> weathers;  // Arraylist for Weater class object
+    WeatherResponse.Main mainClass  ;  // class Main
+    int temparature ;
+
+
     private AutoCompleteTextView autoCompleteTextView;
     ArrayList<String> list;
     ImageView weatherIcon,minTempIcon,maxTempIcon;
@@ -64,12 +73,13 @@ public class MainActivity extends AppCompatActivity {
         weatherResponseCall.enqueue(new Callback<WeatherResponse>() {
             @Override
             public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
-                WeatherResponse weatherResponse = response.body();
-                List<WeatherResponse.Weather> weathers = weatherResponse.getWeather();
-                Log.e("PD DEbug", weathers.get(0).getId()+" , "+ weathers.get(0).getMain() + ", "+ weathers.get
-                        (0).getDescription() );
-                WeatherResponse.Main ma= weatherResponse.getMain();
-                Log.e("Temparature : "," "+ (int)(ma.getTemp()-273.15));
+               weatherResponse = response.body();  // getting the Whole object from the API
+                weathers = weatherResponse.getWeather();  // Arraylist of Weather class object
+                // some test printing
+                Log.e("PD DEbug", weathers.get(0).getId()+" , "+ weathers.get(0).getMain() + ", "+ weathers.get(0).getDescription() );
+                mainClass= weatherResponse.getMain();  // acheiving the Main class
+                temparature = (int)(mainClass.getTemp()-273.15);  // Acheving the temparature in Celcious
+                Log.e("Temparature : "," "+ temparature);
                 initilizereference();
                 minTempIcon.setImageResource(R.drawable.downward);
                 maxTempIcon.setImageResource(R.drawable.upward);
